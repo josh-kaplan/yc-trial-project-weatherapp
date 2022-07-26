2ed9211965d4f5fdf769805d742394c84165910a
Josh Kaplan <josh-kaplan@users.noreply.github.com>
2022-07-26 11:30:44 -0400

    Added front-end basics

--- server.ts
+++ server.ts
@@ -1,12 +1,28 @@
 import express from "express";
+import path from "path";
 import { WeatherApi } from "./src/weatherGovApi";
 const app = express();
 const port = 8080; // default port to listen
 
+// Define the front end routes
+const webAppDir = path.join(__dirname, "webapp")
+app.use(express.static(webAppDir))
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(webAppDir, "index.html"));
+});
+
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
+app.get("/api", ( req, res ) => {
     res.send( "Hello world!" );
 } );
+
+
+// Catch-all route sends all non-valid routes to the React root and the
+// browser router will determine whether or not to show a 404 page.
+app.get("*", (req, res) => {
+    res.sendFile(path.join(webAppDir, "index.html"));
+});
 
 // Uncomment these to fetch data!
 // WeatherApi.point(28.5, -81.4);
