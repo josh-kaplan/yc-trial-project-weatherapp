022c66f42a879c0e2529e61650a1e2d1832ff4f1
Josh Kaplan <josh-kaplan@users.noreply.github.com>
2022-07-26 13:45:12 -0400

    Added API interface with comments

--- server.ts
+++ server.ts
@@ -17,6 +17,40 @@ app.get("/api", ( req, res ) => {
     res.send( "Hello world!" );
 } );
 
+/**
+ * Given an activity like this:
+ *    {
+ *       "name": "",
+ *       "location": [lat, lon],
+ *       fields: {
+ *           "temperature": {
+ *               ideal: { min: 40, max: 70 } 
+ *               acceptable: { min: 0, max: 90 }  
+ *           },
+ *           "windSpeed": {
+ *               ideal: { min: 0, max: 5 } 
+ *               acceptable: { min: 0, max: 20 }  
+ *           }
+ *       }
+ *    }
+ *
+ * Compute a score (method TBD), that says how good it is to do the activity
+ * 
+ * {
+ *    scores: [
+ *         startTime: "2022-07-22T16:53:00+00:00"
+ *         overallScore: 95,
+ *         fields: {
+ *           "temperature": 20
+ *           "windSpeed": 12
+ *         }
+ *    ]
+ * }
+ */
+app.post("/api/activity", ( req, res ) => {
+    res.send( "Hello world!" );
+} );
+
 
 // Catch-all route sends all non-valid routes to the React root and the
 // browser router will determine whether or not to show a 404 page.
